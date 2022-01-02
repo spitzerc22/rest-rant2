f4a8c6ff1cec3a4854526baef7cf52ec0715b9cc
spitzerc22 <spitzerc22@gmail.com>
2022-01-02 12:16:09 -0600

    added instance method, but data is not presenting on index page

--- models/places.js
+++ models/places.js
@@ -12,6 +12,11 @@ const placeSchema = new Schema ({
    founded: Number
 })
 
+//Helper Method
+placeSchema.methods.showEstablished = function() {
+    return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}`
+}
+
 //EXPORT MODEL('Place' is collection, singular, uppercase use 'placeSchema' as guide)
 const Place = mongoose.model('Place', placeSchema)
 
--- views/Show.jsx
+++ views/Show.jsx
@@ -8,20 +8,20 @@ function Show (data) {
                 <h1>{data.place.name}</h1>
             </main>
             <div className="container">
-                <span>
+                <div>
                     <img src={data.place.pic} alt={data.place.name} />
-                </span>
-                <span>
-                    <div>
-                        <h3>Rating</h3>
-                        <p>Not Rated</p>
-                    </div>
-                    <div>
-                        <h3>Description</h3>
-                        <p>Located in {data.place.city}, {data.place.state}</p>
-                        <p>With a {data.place.cuisines} style!</p>
-                    </div>
-                </span>
+                    <p>Located in {data.place.city}, {data.place.state}</p>
+                </div>
+                <div>
+                    <h2>Description</h2>
+                    <h3>{data.Place.showEstablished()}</h3>
+                    <h3>Serving {data.Place.cuisines}</h3>
+                
+                </div>
+                <div>
+                    <h3>Rating</h3>
+                    <p>Not Rated</p>
+                </div>
                 <div>
                     <h3>Comments</h3>
                     <p>No comments yet!</p>
